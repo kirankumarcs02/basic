4e8339a6141669b0a1b6e2e6728bea8b7b67822c
kirankumarcs02 <kirankumarcs02@gmail.com>
2019-04-08 19:21:03 +0530

    if condition examples

---
+++ python/basic/condition_loop/if_condition.py
@@ -0,0 +1,21 @@
+a=3
+b=4
+c=3
+
+print('A = ', a)
+print('B =', b)
+print('C = ',c)
+if a ==c:
+    print('A == C')
+
+if a >b :
+    print("A > B is true")
+else:
+    print('A > B is false')
+
+if c>b:
+    print('C > B is true')
+elif c==3:
+    print('C = 3')
+else:
+    print('all condition are false')
